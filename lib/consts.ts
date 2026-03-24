b92150f03e965c794fb6f3c8dcb50045e75d1a20
Jitse De Smet <jitse@reedonk8.be>
2026-03-24 16:13:32 +0100

    add isBlank rewriting

--- lib/consts.ts
+++ lib/consts.ts
@@ -1,2 +1,3 @@
 export const DT_INTERNAL_BNODE = 'https://sparql-extension.knows.idlab.ugent.be/bnode';
 export const EXTENSION_FUNCTION_BNODE = 'internal://blank';
+export const IRI_PREFIX_BNODE = 'https://myInternalBnode.example.org/';
--- lib/transformations/bnodeMapAsLiteral.ts
+++ lib/transformations/bnodeMapAsLiteral.ts
@@ -2,7 +2,7 @@ import type * as RDF from '@rdfjs/types';
 import type { Algebra } from '@traqula/algebra-transformations-1-2';
 import { algebraUtils } from '@traqula/algebra-transformations-1-2';
 import type { DataFactory } from 'rdf-data-factory';
-import { DT_INTERNAL_BNODE, EXTENSION_FUNCTION_BNODE } from '../consts.js';
+import { DT_INTERNAL_BNODE, EXTENSION_FUNCTION_BNODE, IRI_PREFIX_BNODE } from '../consts.js';
 import type { TransformContext } from '../transformContext.js';
 
 /**
@@ -11,9 +11,100 @@ import type { TransformContext } from '../transformContext.js';
  * rewrite to:
  *   STRDT( ?m0_s, ?m0p,  <internal://bnodetype>)
  *   STRDT( CONCAT ( STR (?s), STR(?p) ), <internal://bnodetype> )
- * @param c
- * @param op
  */
+function expressionForConsistentConstruction(
+  c: TransformContext,
+  expression: Algebra.NamedExpression,
+  hashFunc?: 'md5' | 'sha1' | 'sha256' | 'sha384' | 'sha512',
+): Algebra.Expression {
+  const { AF, DF } = c;
+
+  /// ================== Helper functions ==============================
+  function expressionLiteral(...args: Parameters<(typeof DataFactory)['prototype']['literal']>):
+  Algebra.TermExpression {
+    return AF.createTermExpression(DF.literal(...args));
+  }
+
+  // Return a big if
+  function mapVariable(variable: RDF.Variable): Algebra.Expression {
+    const varAsExpr = AF.createTermExpression(variable);
+    // We assume the var is never bound to a bnode since we explicitly disallow BNODES.
+    return AF.createOperatorExpression('if', [
+      AF.createOperatorExpression('isiri', [ varAsExpr ]),
+      AF.createOperatorExpression('concat', [
+        expressionLiteral(',iri,'),
+        escapeUserInput(AF.createOperatorExpression('str', [ varAsExpr ])),
+      ]),
+      AF.createOperatorExpression('if', [
+        // We now know it is a literal
+        AF.createOperatorExpression('haslangdir', [ varAsExpr ]),
+        AF.createOperatorExpression('concat', [
+          expressionLiteral(',literal@D,'),
+          escapeUserInput(AF.createOperatorExpression('str', [ varAsExpr ])),
+          escapeUserInput(AF.createOperatorExpression('lang', [ varAsExpr ])),
+          escapeUserInput(AF.createOperatorExpression('langdir', [ varAsExpr ])),
+        ]),
+        AF.createOperatorExpression('if', [
+          // We now know it is a literal
+          AF.createOperatorExpression('haslang', [ varAsExpr ]),
+          AF.createOperatorExpression('concat', [
+            expressionLiteral(',literal@,'),
+            escapeUserInput(AF.createOperatorExpression('str', [ varAsExpr ])),
+            escapeUserInput(AF.createOperatorExpression('lang', [ varAsExpr ])),
+          ]),
+          AF.createOperatorExpression('concat', [
+            expressionLiteral(',literal,'),
+            escapeUserInput(AF.createOperatorExpression('str', [ varAsExpr ])),
+            escapeUserInput(AF.createOperatorExpression('str', [
+              AF.createOperatorExpression('datatype', [ varAsExpr ]),
+            ])),
+          ]),
+        ]),
+      ]),
+    ]);
+  }
+
+  /**
+   * Assert that the input does not use our value separator: `,` -> `\,`
+   * Know that this means a string should not end in `\` since then we would also get `\,`
+   */
+  function escapeUserInput(val: Algebra.Expression): Algebra.Expression {
+    const backslashEscaped = AF.createOperatorExpression('replace', [
+      val,
+      // Regex: matches one backslash
+      expressionLiteral('\\\\'),
+      // Replacement: outputs two backslashes
+      expressionLiteral('\\\\\\\\'),
+    ]);
+    return AF.createOperatorExpression('replace', [
+      backslashEscaped,
+      expressionLiteral(','),
+      expressionLiteral('\\\\,'),
+    ]);
+  }
+
+  // ====================== Implementation ========================
+
+  // Ignore non matching
+  if (expression.name.value !== EXTENSION_FUNCTION_BNODE) {
+    return expression;
+  }
+
+  // In case no vars are provided, we always make a new one (you likely never want this though....)
+  let value = AF.createOperatorExpression('struuid', []);
+  if (expression.args.length > 0) {
+    // Make a big concat of conditionals
+    value = AF.createOperatorExpression('concat', (<Algebra.TermExpression[]>expression.args)
+      .sort((a, b) => (<RDF.Variable> a.term).value.localeCompare((<RDF.Variable> b.term).value))
+      .map(expression => mapVariable(<RDF.Variable> expression.term)));
+  }
+
+  if (!hashFunc) {
+    return value;
+  }
+  return AF.createOperatorExpression(hashFunc, [ value ]);
+}
+
 export function internalBnodeAsSpecialLiteral<T extends Algebra.Operation>(c: TransformContext, op: T): T {
   const { AF, DF } = c;
   return algebraUtils.mapOperationSub<'unsafe', typeof op>(
@@ -21,92 +112,63 @@ export function internalBnodeAsSpecialLiteral<T extends Algebra.Operation>(c: Tr
     {},
     { expression: { named: {
       transform: (expression) => {
-        /// ================== Helper functions ==============================
-        function expressionLiteral(...args: Parameters<(typeof DataFactory)['prototype']['literal']>):
-        Algebra.TermExpression {
-          return AF.createTermExpression(DF.literal(...args));
-        }
-
-        // Return a big if
-        function mapVariable(variable: RDF.Variable): Algebra.Expression {
-          const varAsExpr = AF.createTermExpression(variable);
-          // We assume the var is never bound to a bnode since we explicitly disallow BNODES.
-          return AF.createOperatorExpression('if', [
-            AF.createOperatorExpression('isiri', [ varAsExpr ]),
-            AF.createOperatorExpression('concat', [
-              expressionLiteral(',iri,'),
-              escapeUserInput(AF.createOperatorExpression('str', [ varAsExpr ])),
-            ]),
-            AF.createOperatorExpression('if', [
-              // We now know it is a literal
-              AF.createOperatorExpression('haslangdir', [ varAsExpr ]),
-              AF.createOperatorExpression('concat', [
-                expressionLiteral(',literal@D,'),
-                escapeUserInput(AF.createOperatorExpression('str', [ varAsExpr ])),
-                escapeUserInput(AF.createOperatorExpression('lang', [ varAsExpr ])),
-                escapeUserInput(AF.createOperatorExpression('langdir', [ varAsExpr ])),
-              ]),
-              AF.createOperatorExpression('if', [
-                // We now know it is a literal
-                AF.createOperatorExpression('haslang', [ varAsExpr ]),
-                AF.createOperatorExpression('concat', [
-                  expressionLiteral(',literal@,'),
-                  escapeUserInput(AF.createOperatorExpression('str', [ varAsExpr ])),
-                  escapeUserInput(AF.createOperatorExpression('lang', [ varAsExpr ])),
-                ]),
-                AF.createOperatorExpression('concat', [
-                  expressionLiteral(',literal,'),
-                  escapeUserInput(AF.createOperatorExpression('str', [ varAsExpr ])),
-                  escapeUserInput(AF.createOperatorExpression('str', [
-                    AF.createOperatorExpression('datatype', [ varAsExpr ]),
-                  ])),
-                ]),
-              ]),
-            ]),
-          ]);
-        }
-
-        /**
-         * Assert that the input does not use our value separator: `,` -> `\,`
-         * Know that this means a string should not end in `\` since then we would also get `\,`
-         */
-        function escapeUserInput(val: Algebra.Expression): Algebra.Expression {
-          const backslashEscaped = AF.createOperatorExpression('replace', [
-            val,
-            // Regex: matches one backslash
-            expressionLiteral('\\\\'),
-            // Replacement: outputs two backslashes
-            expressionLiteral('\\\\\\\\'),
-          ]);
-          return AF.createOperatorExpression('replace', [
-            backslashEscaped,
-            expressionLiteral(','),
-            expressionLiteral('\\\\,'),
-          ]);
-        }
-
-        // ====================== Implementation ========================
-
-        // Ignore non matching
-        if (expression.name.value !== EXTENSION_FUNCTION_BNODE) {
+        const value = expressionForConsistentConstruction(c, expression);
+        if (expression === value) {
           return expression;
         }
-
-        // In case no vars are provided, we always make a new one (you likely never want this though....)
-        let value = AF.createOperatorExpression('struuid', []);
-        if (expression.args.length > 0) {
-          // Make a big concat of conditionals
-          value = AF.createOperatorExpression('concat', (<Algebra.TermExpression[]>expression.args)
-            .sort((a, b) => (<RDF.Variable> a.term).value.localeCompare((<RDF.Variable> b.term).value))
-            .map(expression => mapVariable(<RDF.Variable> expression.term)));
-        }
-
         return AF.createOperatorExpression('strdt', [
           value,
           AF.createTermExpression(DF.namedNode(DT_INTERNAL_BNODE)),
         ]);
       },
+    }, operator: {
+      transform: (expression) => {
+        if (expression.operator !== 'isblank') {
+          return expression;
+        }
+
+        return AF.createOperatorExpression('=', [
+          AF.createOperatorExpression('datatype', expression.args),
+          AF.createTermExpression(DF.literal(DT_INTERNAL_BNODE)),
+        ]);
+      },
     }}},
   );
-  // Now also write conditions
+}
+
+export function internalBnodeAsSpecialIri<T extends Algebra.Operation>(c: TransformContext, op: T): T {
+  const { AF, DF } = c;
+  return algebraUtils.mapOperationSub<'unsafe', typeof op>(
+    op,
+    {},
+    { expression: { named: {
+      transform: (expression) => {
+        const value = expressionForConsistentConstruction(c, expression, 'sha1');
+        if (expression === value) {
+          return expression;
+        }
+        return AF.createOperatorExpression('iri', [
+          AF.createOperatorExpression('concat', [
+            AF.createTermExpression(DF.literal(IRI_PREFIX_BNODE)),
+            value,
+          ]),
+        ]);
+      },
+    }, operator: {
+      transform: (expression) => {
+        if (expression.operator !== 'isblank') {
+          return expression;
+        }
+
+        return AF.createOperatorExpression('&&', [
+          AF.createOperatorExpression('isiri', expression.args),
+
+          AF.createOperatorExpression('STRSTARTS', [
+            AF.createOperatorExpression('str', expression.args),
+            AF.createTermExpression(DF.literal(IRI_PREFIX_BNODE)),
+          ]),
+        ]);
+      },
+    }}},
+  );
 }
